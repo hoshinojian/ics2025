8079913ad4c751f4b8a2db570a4f936bc9d078c4
tracer-ics2025 <tracer@njuics.org>
2026-01-18 18:40:53 +0800

    >  compile NEMU 20010919 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:40:53 up  7:32,  1 user,  load average: 0.16, 0.11, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -95,6 +95,12 @@ static int cmd_x(char *args){
   return 0;
 }
 
+static int cmd_p(char *args){
+  if(args == NULL)return 0;
+  bool success = true;
+  return expr(args,&success);
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -106,7 +112,7 @@ static struct {
   { "si", "Program by steps as the form of \"si [x]\", the default x is 1", cmd_si},
   { "info", "Print the state of the program. Eg: info r will print the REGS, and info w will print the info of watchpoints.", cmd_info },
   { "x", "Use the command in form of \"x N EXPR\", Eg:x 4 1+1. Calculate the expr, use the ans as the beginner position of mm, then print the following 4 * N Bytes. ", cmd_x},
-  
+  { " p", "Caculator", cmd_p},
   /* TODO: Add more commands */
 
 };
