b03de2c189825bec68b482198932bac4064d2133
tracer-ics2025 <tracer@njuics.org>
2026-01-18 18:36:18 +0800

    >  compile NEMU 20010919 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:36:18 up  7:28,  1 user,  load average: 0.10, 0.09, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -14,6 +14,7 @@
  ***************************************************************************************/
 
 #include <isa.h>
+#include <stdio.h>
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
@@ -22,7 +23,7 @@
 
 enum
 {
-  TK_NOTYPE = 256,
+  TK_NOTYPE = 0,
 
   TK_DEC,
 
@@ -31,6 +32,9 @@ enum
   TK_MIN,
   TK_MUL,
   TK_DIV,
+
+  TK_LEFT,
+  TK_RIGHT,
 
   /* TODO: Add more token types */
 
@@ -52,6 +56,9 @@ static struct rule
     {"\\*", TK_MUL},    // plus
     {"\\/", TK_DIV},    // plus
     {"[0-9]+", TK_DEC}, // plus
+
+    {"\\(", TK_LEFT},
+    {"\\)", TK_RIGHT},
 
     {"==", TK_EQ}, // equal
 };
@@ -88,7 +95,7 @@ typedef struct token
   char str[32];
 } Token;
 
-static Token tokens[1024] __attribute__((used)) = {};
+static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used)) = 0;
 
 static bool make_token(char *e)
@@ -124,27 +131,24 @@ static bool make_token(char *e)
         {
         case TK_NOTYPE:
           break;
+
         case TK_ADD:
-          tokens[nr_token].type = TK_ADD;
-          nr_token++;
-          break;
         case TK_MIN:
-          tokens[nr_token].type = TK_MIN;
-          nr_token++;
-          break;
         case TK_MUL:
-          tokens[nr_token].type = TK_MUL;
-          nr_token++;
-          break;
         case TK_DIV:
-          tokens[nr_token].type = TK_DIV;
+        case TK_EQ:
+        case TK_LEFT:
+        case TK_RIGHT:
+          tokens[nr_token].type = rules[i].token_type;
           nr_token++;
           break;
+
         case TK_DEC:
           tokens[nr_token].type = TK_DEC;
-          strncpy(tokens[nr_token].str, substr_start,substr_len);
+          strncpy(tokens[nr_token].str, substr_start, substr_len);
+          tokens[nr_token].str[substr_len] = '\0';
           nr_token++;
-
+          break;
         default:
           TODO();
         }
@@ -163,6 +167,89 @@ static bool make_token(char *e)
   return true;
 }
 
+int numstk[32];
+int numstktop = 0;
+
+int opstk[32];
+int opstktop = 0;
+
+static void numspush(int n) { numstk[numstktop++] = n; }
+static int numspop() { return numstk[--numstktop]; }
+
+static void oppush(int n) { opstk[opstktop++] = n; }
+static int oppop() { return opstk[--opstktop]; }
+static int optop() {return opstk[opstktop-1];}
+
+static void calc()
+{
+  int b = numspop();
+  int a = numspop();
+  int op = oppop();
+  int ans = 0;
+  switch (op)
+  {
+  case TK_ADD:
+    ans = a + b;
+    break;
+  case TK_MIN:
+    ans = a - b;
+    break;
+  case TK_MUL:
+    ans = a * b;
+    break;
+  case TK_DIV:
+    if (b == 0)
+    {
+      printf("Error: Division by zero\n");
+      return;
+    }
+    ans = a / b;
+    break;
+  case TK_EQ:
+    ans = (a == b);
+    break;
+  default:
+    break;
+  }
+  numspush(ans);
+}
+
+static int op_to_idx(int token_type) {
+    switch (token_type) {
+        case TK_ADD: return 0; // 对应矩阵第 0 行/列
+        case TK_MIN: return 1; // 对应矩阵第 1 行/列
+        case TK_MUL: return 2; // 对应矩阵第 2 行/列
+        case TK_DIV: return 3; // 对应矩阵第 3 行/列
+        case TK_LEFT: return 4; // 对应 (
+        case TK_RIGHT: return 5; // 对应 )
+        default: 
+            // 遇到数字或者错误的符号，返回一个非法下标
+            // 你的矩阵是 10x10，所以返回 9 是安全的（前提是你矩阵填满了0）
+            return 9; 
+    }
+}
+
+
+
+static char pri[10][10] = {
+    //左侧意味着栈顶,右侧意味seq. 1意味着栈顶先运算, 0意味入栈
+  //如果是0意味着非法, 如果是~意味着左括号出站. //右括号不可能在栈顶
+    //              +    -    * /    (    )    ...填充0...
+    /* + (0) */ { '>', '>', '<', '<', '<', '>', '0', '0', '0', '0' },
+    /* - (1) */ { '>', '>', '<', '<', '<', '>', '0', '0', '0', '0' },
+    /* * (2) */ { '>', '>', '>', '>', '<', '>', '0', '0', '0', '0' },
+    /* / (3) */ { '>', '>', '>', '>', '<', '>', '0', '0', '0', '0' },
+    /* ( (4) */ { '<', '<', '<', '<', '<', '~', '0', '0', '0', '0' },
+    /* ) (5) */ { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' }, 
+};
+
+//输入的是两个enum下来的数值
+static char priority(int stacktopop, int seqop){
+  int p = op_to_idx(stacktopop);
+  int q = op_to_idx(seqop);
+  return pri[p][q];
+}
+
 word_t expr(char *e, bool *success)
 {
   if (!make_token(e))
@@ -173,6 +260,39 @@ word_t expr(char *e, bool *success)
 
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
+  numstktop = 0;
+  opstktop = 0;
+  *success = true;
 
-  return 0;
+
+  for (int i = 0; i < nr_token; i++)
+  {
+    // 如果这个token的type是数字
+    if (tokens[i].type == TK_DEC)
+    {
+      int num = atoi(tokens[i].str);
+      numspush(num);
+    }
+    // 这个token的type是符号
+    else
+    {
+      while(opstktop > 0){ // 只要stk不空, 就一直比较
+        char rel = priority(optop(), tokens[i].type);
+        if(rel == '<'){//seq的优先级比栈顶的更高, 比如+ *
+          oppush(tokens[i].type);
+        }else if(rel == '>'){//可以计算
+          calc();
+        }else if(rel == '~'){//左括号碰见右括号
+          oppop();
+        }else if(rel == '0'){//非法
+          *success = false;
+          return 0;
+        }
+      }
+      oppush(tokens[i].type);//这个时候stk空
+    }
+
+  }
+
+  return numstk[0];
 }
