0d1449b3d635e8981943cbcb6786f9ed1530c4ce
tracer-ics2025 <tracer@njuics.org>
2026-01-20 02:17:42 +0800

    >  compile NEMU 20010919 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:17:42 up 15:22,  1 user,  load average: 0.22, 0.19, 0.18

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -28,6 +28,7 @@ typedef struct
 
 void TESTCALC()
 {
+  Log("TEST START.\n");
   FILE *fp = fopen("./tools/gen-expr/build/input", "r");
   assert(fp != NULL);
   // fp里面每一行, 前面一个是结果后面一个是式子
@@ -65,7 +66,8 @@ void TESTCALC()
     i++;
   }
   fclose(FP);
-  printf("%d ERROR.\n", WA);
+    Log("TEST ENDS WITH %d ERRORS.\n", WA);
+
   free(points);
 }
 
