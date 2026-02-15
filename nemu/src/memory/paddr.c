5a51e18ee1653071151edd7d84ed80f0709a3a48
tracer-ics2025 <tracer@njuics.org>
2026-02-15 18:35:05 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:35:05 up  2:22,  1 user,  load average: 0.00, 0.03, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -34,8 +34,6 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 //haddr-pmem计算当前指针到开头的距离, 加上MBASE对应到nemu地址
 
 
-//todo
-
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
