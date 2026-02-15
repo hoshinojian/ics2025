e6f40718dcd770e19babb778d5d336193e397e5e
tracer-ics2025 <tracer@njuics.org>
2026-02-15 16:31:33 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:31:33 up 30 min,  1 user,  load average: 0.07, 0.07, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,7 +40,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  if (g_print_step) { //IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); 
+  }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
   //扫描所有的监视点, 可能多个监视点同时改变. 如果有改变的, 停止. 
