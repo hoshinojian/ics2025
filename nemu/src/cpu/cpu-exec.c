0ffefdc1a6a55af757ee4fb0eef3511b3db75245
tracer-ics2025 <tracer@njuics.org>
2026-02-15 17:31:27 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:31:27 up  1:25,  1 user,  load average: 0.21, 0.11, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -165,7 +165,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-          assert_fail_msg();
+          
 
 
       Log("nemu: %s at pc = " FMT_WORD,
