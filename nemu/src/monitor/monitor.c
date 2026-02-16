89ecf549fd47f84b33b0e3aa72d4f145d3bbfb7b
tracer-ics2025 <tracer@njuics.org>
2026-02-16 12:49:56 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:49:56 up 23 min,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -127,7 +127,8 @@ void init_monitor(int argc, char *argv[]) {
 
   //todo
   //在这里插入对于mtrace的定义
-  IFDEF(CONFIG_MM_TRACE, init_mm_log());
+  #define CONFIG_MM_TRACE 1
+  IFDEF(CONFIG_MM_TRACE, init_mm_log(mm_log_file));
 
   /* Initialize memory. */
   init_mem();
