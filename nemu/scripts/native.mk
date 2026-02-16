21302a88a31fc3b96ddd680bb38e84c748d61ce2
tracer-ics2025 <tracer@njuics.org>
2026-02-16 10:43:01 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:43:01 up  8:30,  1 user,  load average: 0.06, 0.05, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,7 @@ $(BINARY):: compile_git
 
 # override ARGS ?= --batch
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
-override ARGS ?= --mmlog=$(BUILD_DIR)/mmlog.txt
+override ARGS += --mmlog=$(BUILD_DIR)/mmlog.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
