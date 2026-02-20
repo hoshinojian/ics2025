d3a27f7fc460ba772580769997e63ec5bb414f10
tracer-ics2025 <tracer@njuics.org>
2026-02-20 19:33:01 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:33:01 up 13:56,  1 user,  load average: 0.00, 0.02, 0.01

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -109,11 +109,10 @@ static int parse_args(int argc, char *argv[]) {
       case 'f': func_log_file = optarg; break;
       case 'e': {elf_file = optarg; 
                 if (elf_file != NULL) {
-    printf("【FTRACE 成功】我拿到的 ELF 路径是: %s\n", elf_file);
-} else {
-    printf("【FTRACE 失败】报告，elf_file 还是 NULL！\n");
-}
-break;
+                printf("【FTRACE 成功】我拿到的 ELF 路径是: %s\n", elf_file);
+              } else {
+                printf("【FTRACE 失败】报告，elf_file 还是 NULL！\n");
+              }break;
       }
       
       case 1: img_file = optarg; return 0;
