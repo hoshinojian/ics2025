0c3bebf91f902d4d5551d4cfe89cb913c08cc627
tracer-ics2025 <tracer@njuics.org>
2026-02-21 22:01:19 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:01:19 up 11:00,  1 user,  load average: 0.10, 0.10, 0.05

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,8 +17,23 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+extern CPU_state cpu;
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return 1;
+  bool flag = true;
+  for(int i = 1; i < 31; i++){
+    if(ref_r->gpr[i]!=cpu.gpr[i]){
+      Log("Difftest error at PC = 0x%08x", pc);
+      Log("Register [%d] mismatch: NEMU = 0x%08x, REF(Spike) = 0x%08x", 
+          i, cpu.gpr[i], ref_r->gpr[i]);
+      flag = false;
+    }
+  }
+  if (cpu.pc != ref_r->pc) {
+    Log("Difftest error at PC mismatch!");
+    Log("NEMU PC = 0x%08x, REF(Spike) PC = 0x%08x", cpu.pc, ref_r->pc);
+    flag = false;
+  }
+  return flag;
 }
 
 void isa_difftest_attach() {
