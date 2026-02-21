bf9c1baa6db7e06c7fbaec4322a4570775d1bb22
tracer-ics2025 <tracer@njuics.org>
2026-02-21 14:30:32 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:30:32 up  3:52,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/include/log.h
+++ nemu/include/log.h
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#ifdef CONFIG_FUNC_TRACE
 // 定义函数信息结构体
 typedef struct {
   char* func_name;
@@ -16,31 +17,35 @@ typedef struct {
 } FuncInfo;
 
 // 暴露的全局文件指针声明
-extern FILE *log_fp;
 extern FILE *func_log_fp;
-
-#ifdef CONFIG_MM_TRACE
-extern FILE *mm_log_fp;
-#endif
 
 // 暴露的 ELF 节头表声明
 extern Elf32_Shdr symtab_shdr;
 extern Elf32_Shdr strtab_shdr;
 
-// 核心函数声明
-void init_log(const char *log_file);
-bool log_enable(void);
+// 暴露解析出的函数数组和数量，供外部的 ftrace 使用
+extern FuncInfo* funcinfo;
+extern int global_func_count;
 
+// 核心函数声明
 void parse_elf(const char *elf_file);
 FuncInfo* func_addr(Elf32_Shdr symtab_shdr, Elf32_Shdr strtab_shdr, const char *elf_file, int *out_func_count);
 void init_func_log(const char *func_log_file, const char *elf_file);
+#endif
+
+// 暴露的全局文件指针声明
+extern FILE *log_fp;
+
+#ifdef CONFIG_MM_TRACE
+extern FILE *mm_log_fp;
+#endif
+
+// 核心函数声明
+void init_log(const char *log_file);
+bool log_enable(void);
 
 #ifdef CONFIG_MM_TRACE
 void init_mm_log(const char *mm_log_file);
 #endif
 
 #endif // __ELF_LOG_H__
-
-extern FuncInfo* funcinfo;
-extern int global_func_count;
-
