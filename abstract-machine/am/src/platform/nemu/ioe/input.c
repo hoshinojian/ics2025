0dff6a75a3a5d9f010e9c37fd4a71925c63b2d9a
tracer-ics2025 <tracer@njuics.org>
2026-02-26 16:40:26 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:40:26 up 2 days,  4:41,  1 user,  load average: 0.26, 0.25, 0.16

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,7 +6,7 @@
   // AM_INPUT_KEYBRD, AM键盘控制器, 可读出按键信息. keydown为true时表示按下按键, 否则表示释放按键. keycode为按键的断码, 没有按键时, keycode为AM_KEY_NONE.
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t state = inl(KBD_ADDR);
-  if(state >> 31){
+  if(state & KEYDOWN_MASK >> 31){
     kbd->keydown = 1;
     kbd->keycode = state & 0x7fff;
   }else{
