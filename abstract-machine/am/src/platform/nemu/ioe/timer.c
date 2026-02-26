fbf1f0bff2b2e9b99818d5f83a6f9c539a24f3eb
tracer-ics2025 <tracer@njuics.org>
2026-02-26 11:13:40 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:13:40 up 1 day, 22:51,  1 user,  load average: 0.03, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -10,16 +10,14 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //假设io寄存器这个时候已经有了数据, 那么直接从io寄存器取到总线上
-  inl(RTC_ADDR);
+  uint64_t low = inl(RTC_ADDR);
+  uint64_t high = inl(RTC_ADDR + 4);
   //然后输出就好了
 
-  uptime->us = 0;
+  uptime->us = (high << 32) + low;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-
-  
-
   rtc->second = 0;
   rtc->minute = 0;
   rtc->hour   = 0;
