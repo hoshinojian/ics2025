d2526576ab35e1a47ad4df39e1f1b9647572d683
tracer-ics2025 <tracer@njuics.org>
2026-03-15 17:53:40 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:53:40 up 1 day,  2:36,  1 user,  load average: 0.08, 0.07, 0.01

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -17,7 +17,6 @@ YELLOW := $(shell printf "\033[33m")
 
 NONE := $(shell printf "\033[0m")
 
-$(info $(YELLOW)file: NEMU/native is used$(NONE))
 $(info $(YELLOW)file: NEMU/makefile is used $(NONE))
 
 
