44a05009ce7019e239fc585f21c4521d31aa4fed
tracer-ics2025 <tracer@njuics.org>
2026-03-18 21:14:02 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:14:02 up 11:43,  1 user,  load average: 0.03, 0.04, 0.00

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -42,8 +42,9 @@ LDFLAGS := -O2 $(LDFLAGS)
 
 # srcs里面的所有东西全部变成.o形式,丢到objs里面去
 
+
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
-$(info$(GREEN)$(PREFIX_BUILD)$(NONE) OBJS = $(notdir $(OBJS)) $(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE) OBJS = $(notdir $(OBJS)) $(NONE))
 
 # Compilation patterns
 $(OBJ_DIR)/%.o: %.c
