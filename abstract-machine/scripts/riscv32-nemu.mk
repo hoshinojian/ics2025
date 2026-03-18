2dd952b115e604272209ecad760b9e8b2143150d
tracer-ics2025 <tracer@njuics.org>
2026-03-18 12:51:37 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:51:37 up  3:29,  1 user,  load average: 0.07, 0.06, 0.01

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -12,13 +12,22 @@ include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 #define ISA_H "riscv/riscv.h"。
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+$(info $(GREEN)$(PREFIX_riscv32_nemu)$(NONE)CFLAGS = $(CFLAGS))
+
 # 告诉gcc底层硬件的能力: -march=~, 指令集架构. 
 COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32   # overwrite
+$(info $(GREEN)$(PREFIX_riscv32_nemu)$(NONE)COMMON_CFLAGS = $(COMMON_CFLAGS))
+
 # 最后的elf必须是32位, 小端序, riscv格式
 LDFLAGS       += -melf32lriscv                     # overwrite
+$(info $(GREEN)$(PREFIX_riscv32_nemu)$(NONE)LDFLAGS = $(LDFLAGS))
+
 
 # /home/xingjian/ics2025/abstract-machine/am/src/riscv/nemu/start.S
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
            riscv/nemu/trap.S \
            riscv/nemu/vme.c
+           
+$(info $(GREEN)$(PREFIX_riscv32_nemu)$(NONE)AM_SRCS = $(AM_SRCS))
+
