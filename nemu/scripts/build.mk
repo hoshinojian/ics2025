d334bdfc5a2cbb74f9768ecf66d0dbdafa021750
tracer-ics2025 <tracer@njuics.org>
2026-03-18 20:44:41 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:44:41 up 11:13,  1 user,  load average: 0.02, 0.07, 0.02

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -16,18 +16,18 @@ LDFLAGS += -shared -fPIC
 endif
 
 WORK_DIR  = $(shell pwd)
-$(info $(YELLOW)$(PREFIX_BUILD)WORK_DIR is $(WORK_DIR)$(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE)WORK_DIR is $(WORK_DIR)$(NONE))
 BUILD_DIR = $(WORK_DIR)/build
-$(info $(YELLOW)$(PREFIX_BUILD)BUILD_DIR is $(BUILD_DIR)$(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE)BUILD_DIR is $(BUILD_DIR)$(NONE))
 
 INC_PATH := $(WORK_DIR)/include $(INC_PATH)
-$(info $(YELLOW)$(PREFIX_BUILD)INC_PATH is $(INC_PATH)$(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE)INC_PATH is $(INC_PATH)$(NONE))
 
 OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
-$(info $(YELLOW)$(PREFIX_BUILD)OBJ_DIR is $(OBJ_DIR)$(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE)OBJ_DIR is $(OBJ_DIR)$(NONE))
 
 BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
-$(info $(YELLOW)$(PREFIX_BUILD)BIINARY is $(BINARY)$(NONE))
+$(info $(GREEN)$(PREFIX_BUILD)$(NONE)BIINARY is $(BINARY)$(NONE))
 
 # Compilation flags
 ifeq ($(CC),clang)
@@ -69,7 +69,7 @@ app: $(BINARY)
 $(BINARY):: $(OBJS) $(ARCHIVES)
 	@echo + LD $@
 	@$(LD) -o $@ $(OBJS) $(LDFLAGS) $(ARCHIVES) $(LIBS)
-	$($(YELLOW)$(PREFIX_BUILD)info the command Binary(build.mk) is used$(NONE))
+	$(info $(GREEN)$(PREFIX_BUILD)$(NONE)the command Binary(build.mk) is used$(NONE))
 
 clean:
 	-rm -rf $(BUILD_DIR)
--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -19,6 +19,7 @@ PREFIX_NATIVE := [=== NEMU / NATIVE.MK ===]
 $(info $(NONE))
 $(info $(YELLOW)$(PREFIX_NATIVE)$(NONE))
 $(info $(NONE))
+
 -include $(NEMU_HOME)/../Makefile
 include $(NEMU_HOME)/scripts/build.mk
 
