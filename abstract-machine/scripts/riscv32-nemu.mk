1e18fd7b6135e3d29b5ee1453da2d4e3e437a084
tracer-ics2025 <tracer@njuics.org>
2026-03-18 12:45:52 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:45:52 up  3:23,  1 user,  load average: 0.16, 0.06, 0.01

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -6,6 +6,10 @@ NONE   := $(shell printf "\033[0m")
 PREFIX_riscv32_nemu := [=== AM/riscv32-nemu.mk ===]
 
 $(info$(YELLOW)$(PREFIX_riscv32_nemu)$(NONE))
+$(info$(YELLOW)$(PREFIX_riscv32_nemu)$(NONE))
+$(info$(YELLOW)$(PREFIX_riscv32_nemu)$(NONE))
+$(info$(YELLOW)$(PREFIX_riscv32_nemu)$(NONE))
+$(info$(YELLOW)$(PREFIX_riscv32_nemu)$(NONE))
 
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
