e050c4ebcacedbc62c89ab680b9e54776b8e048e
tracer-ics2025 <tracer@njuics.org>
2026-03-18 15:46:01 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:46:01 up  6:19,  1 user,  load average: 0.07, 0.02, 0.00

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -1,3 +1,6 @@
+PREFIX_am_nemu := [=== AM / platform / nemu.mk ===]
+
+$(info $(YELLOW)$(PREFIX_am_nemu)$(NONE))
 AM_SRCS := platform/nemu/trm.c \
            platform/nemu/ioe/ioe.c \
            platform/nemu/ioe/timer.c \
@@ -22,19 +25,32 @@ MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
 CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
+
+$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)MAINARGS_MAX_LEN = $(MAINARGS_MAX_LEN))
+$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)MAINARGS_PLACEHOLDER = $(MAINARGS_PLACEHOLDER))
+$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)CFLAGS = $(CFLAGS))
+
+
 # ??mainargs???
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
+    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command Insert-arg Called)
 
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command image called)
+
 
 run: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command run called)
+
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
+    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command GDB called)
+
 
 .PHONY: insert-arg
