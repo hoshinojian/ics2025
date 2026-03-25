9fed135b8e0339426ce11242e34fac22249dc8b8
tracer-ics2025 <tracer@njuics.org>
2026-03-25 16:58:51 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:58:51 up 1 day,  6:10,  1 user,  load average: 0.01, 0.02, 0.00

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -2,13 +2,13 @@ PREFIX_am_nemu := [=== AM / platform / nemu.mk ===]
 
 $(info $(YELLOW)$(PREFIX_am_nemu)$(NONE))
 AM_SRCS := platform/nemu/trm.c \
-           platform/nemu/ioe/ioe.c \
-           platform/nemu/ioe/timer.c \
-           platform/nemu/ioe/input.c \
-           platform/nemu/ioe/gpu.c \
-           platform/nemu/ioe/audio.c \
-           platform/nemu/ioe/disk.c \
-           platform/nemu/mpe.c
+		   platform/nemu/ioe/ioe.c \
+		   platform/nemu/ioe/timer.c \
+		   platform/nemu/ioe/input.c \
+		   platform/nemu/ioe/gpu.c \
+		   platform/nemu/ioe/audio.c \
+		   platform/nemu/ioe/disk.c \
+		   platform/nemu/mpe.c
 
 # ?????????????????. 
 # ??, ???????????????, ??????text??. ????, ????????????, ????text????????
@@ -40,23 +40,25 @@ CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINAR
 # ??mainargs???
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+
 
 
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
-    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command image called)
+	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command image called)
 
 
 run: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
-    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command run called)
+	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command run called)
 
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
-    $(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command GDB called)
+	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command GDB called)
 
 
 .PHONY: insert-arg
