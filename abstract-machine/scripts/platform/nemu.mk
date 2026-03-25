fd9828fe61a64b7d2d1890fd3d76df76bef2ed3f
tracer-ics2025 <tracer@njuics.org>
2026-03-25 18:28:56 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:28:56 up 1 day,  7:47,  1 user,  load average: 0.00, 0.01, 0.00

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -40,12 +40,7 @@ CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINAR
 # ??mainargs???
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
-	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo "$(GREEN)$(PREFIX_am_nemu)$(NONE) Command Insert-arg Called"
 
 
 
@@ -53,17 +48,16 @@ image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
-	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command image called)
-
+	@echo "$(GREEN)$(PREFIX_am_nemu)$(NONE) Command image called"
 
 run: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
-	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command run called)
+	@echo "$(GREEN)$(PREFIX_am_nemu)$(NONE) Command run called"
 
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
-	$(info $(GREEN)$(PREFIX_am_nemu)$(NONE)Command GDB called)
+	@echo "$(GREEN)$(PREFIX_am_nemu)$(NONE) Command GDB called"
 
 
 .PHONY: insert-arg
