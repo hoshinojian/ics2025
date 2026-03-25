387b970e1aaa3c6375bdd8b3b297cd6467768d84
tracer-ics2025 <tracer@njuics.org>
2026-03-25 16:59:04 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:59:04 up 1 day,  6:10,  1 user,  load average: 0.08, 0.04, 0.00

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -41,6 +41,11 @@ CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINAR
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
+	@echo -s "$(GREEN)$(PREFIX_am_nemu)$(NONE) insert-arg called"
 
 
 
