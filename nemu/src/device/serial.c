c31159c2f8f5868af1be3fecfc95a79c9da6154b
tracer-ics2025 <tracer@njuics.org>
2026-03-27 10:59:33 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:59:33 up 3 days,  1:43,  1 user,  load average: 0.38, 0.16, 0.06

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -25,6 +25,7 @@ static uint8_t *serial_base = NULL;
 
 
 static void serial_putc(char ch) {
+  MUXDEF(CONFIG_TARGET_AM, {printf("CONFIG_TARGET_AM\n");}, {printf("NO_CONFIG_TARGET_AM\n");});
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
 
