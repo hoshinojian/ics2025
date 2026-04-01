ded02932d55d23401b52199298c76a5aeef9502d
tracer-ics2025 <tracer@njuics.org>
2026-04-01 22:59:03 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:59:03 up  3:35,  1 user,  load average: 0.06, 0.09, 0.15

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -330,7 +330,11 @@ static int decode_exec(Decode *s)
     //±£´æpc
     s->dnpc = isa_raise_intr(11, s->pc);
   });
-  INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak, N, {
+    NEMUTRAP(s->pc, R(10));
+    s->dnpc = isa_raise_intr(3, s->pc);
+
+  }); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ???????", inv, N, INV(s->pc));
 
   INSTPAT_END();
