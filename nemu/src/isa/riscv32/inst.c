c50168d27272ccfd2dd8a1c90ce59ccd9fdb404a
tracer-ics2025 <tracer@njuics.org>
2026-04-01 21:59:53 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:59:53 up  2:38,  1 user,  load average: 0.03, 0.10, 0.06

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -25,7 +25,8 @@
 
 #ifdef CONFIG_FUNC_TRACE
 void log_ftrace(int type, uint32_t pc, uint32_t target);
-enum {
+enum
+{
   type_jal,
   type_jalr_call,
   type_jalr_ret
@@ -55,10 +56,10 @@ enum
     *src2 = R(rs2); \
   } while (0)
 
- #define csrI()                       \
-  do                                  \
-  {                                   \
-    *csr = BITS(i, 31, 20);     \
+#define csrI()              \
+  do                        \
+  {                         \
+    *csr = BITS(i, 31, 20); \
   } while (0)
 
 #define immI()                        \
@@ -73,16 +74,14 @@ enum
     *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); \
   } while (0)
 
-
-#define immB()                                       \
-  do                                                 \
-  {                                                  \
-    *imm = (SEXT(BITS(i, 31, 31), 1) << 12) |        \
-           (BITS(i, 7, 7) << 11) |                   \
-           (BITS(i, 30, 25) << 5) |                  \
-           (BITS(i, 11, 8) << 1);                    \
+#define immB()                                \
+  do                                          \
+  {                                           \
+    *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | \
+           (BITS(i, 7, 7) << 11) |            \
+           (BITS(i, 30, 25) << 5) |           \
+           (BITS(i, 11, 8) << 1);             \
   } while (0)
-
 
 #define immU()                              \
   do                                        \
@@ -90,27 +89,25 @@ enum
     *imm = SEXT(BITS(i, 31, 12), 20) << 12; \
   } while (0)
 
-
 /* J-type: imm[20] | imm[10:1] | imm[11] | imm[19:12] | rd | opcode */
 /* Content:  s  |  inst[30:21] | i11 | inst[19:12] | rd | 1101111  */
-#define immJ()                                                \
-  do                                                          \
-  {                                                           \
-    uint32_t i = s->isa.inst;                                 \
-    word_t rs = (BITS(i,31,31) << 20)   |                     \
-                (BITS(i, 19, 12) << 12) |                     \
-                (BITS(i, 20, 20) << 11) |                     \
-                (BITS(i, 30, 21) << 1);                       \
-    *imm = (SEXT(rs, 21));                                    \
+#define immJ()                            \
+  do                                      \
+  {                                       \
+    uint32_t i = s->isa.inst;             \
+    word_t rs = (BITS(i, 31, 31) << 20) | \
+                (BITS(i, 19, 12) << 12) | \
+                (BITS(i, 20, 20) << 11) | \
+                (BITS(i, 30, 21) << 1);   \
+    *imm = (SEXT(rs, 21));                \
   } while (0);
-  
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, word_t *csr, int type)
 {
   uint32_t i = s->isa.inst;
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
-  *rd = BITS(i, 11, 7);   //rd是一个地址 src1 src2都是地址
+  *rd = BITS(i, 11, 7); // rd是一个地址 src1 src2都是地址
   *csr = BITS(i, 31, 20);
   switch (type)
   {
@@ -154,148 +151,166 @@ static int decode_exec(Decode *s)
 {
   s->dnpc = s->snpc;
 
-#define INSTPAT_INST(s) ((s)->isa.inst)   //INSTPAT INST s 代表s的指令
-//instpat_match完成从指令中取出来寄存器、立即数和执行
-#define INSTPAT_MATCH(s, name, type, ... /* execute body */)         \
-  {                                                                  \
-    int rd = 0;                                                      \
-    word_t src1 = 0, src2 = 0, imm = 0, csr = 0;                     \
+#define INSTPAT_INST(s) ((s)->isa.inst) // INSTPAT INST s 代表s的指令
+// instpat_match完成从指令中取出来寄存器、立即数和执行
+#define INSTPAT_MATCH(s, name, type, ... /* execute body */)               \
+  {                                                                        \
+    int rd = 0;                                                            \
+    word_t src1 = 0, src2 = 0, imm = 0, csr = 0;                           \
     decode_operand(s, &rd, &src1, &src2, &imm, &csr, concat(TYPE_, type)); \
-    __VA_ARGS__;                                                     \
+    __VA_ARGS__;                                                           \
   }
   // s: 解码结构体指针
   // name 指令名字
   // type 什么类型的， uisj， 决定了怎么取出来立即数和寄存器索引
-  // ... 可变参数, 接受具体的C代码. 
+  // ... 可变参数, 接受具体的C代码.
   //----------------------------------
-  //decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); 自动解码操作数
-  //concat TYPE_type, 完成自动的拼接, 之后自动完成执行
+  // decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); 自动解码操作数
+  // concat TYPE_type, 完成自动的拼接, 之后自动完成执行
   // __VA_ARGS__; 完成最后 "..."的指令
 
   INSTPAT_START();
-  //INSTPAT(模式字符串, 指令名称, 指令类型, 指令执行操作);
-  INSTPAT("0000000 ????? ????? 000 ????? 0110011", add, R, {R(rd) = src1 + src2;});
+  // INSTPAT(模式字符串, 指令名称, 指令类型, 指令执行操作);
+  INSTPAT("0000000 ????? ????? 000 ????? 0110011", add, R, { R(rd) = src1 + src2; });
   INSTPAT("0100000 ????? ????? 000 ????? 0110011", sub, R, R(rd) = src1 - src2;);
-  INSTPAT("0000000 ????? ????? 001 ????? 0110011", sll, R, {R(rd) = src1 << (src2 & 0x1f);});//逻辑左移最低五个 of src2
+  INSTPAT("0000000 ????? ????? 001 ????? 0110011", sll, R, { R(rd) = src1 << (src2 & 0x1f); }); // 逻辑左移最低五个 of src2
   INSTPAT("0000000 ????? ????? 010 ????? 0110011", slt, R, R(rd) = ((int32_t)src1 < (int32_t)src2););
-  INSTPAT("0000000 ????? ????? 011 ????? 0110011", sltu, R, {R(rd) = ((uint32_t)src1 < (uint32_t)src2);});
-  INSTPAT("0000000 ????? ????? 100 ????? 0110011", xor, R, {R(rd) = src1 ^ src2;});
-  INSTPAT("0000000 ????? ????? 101 ????? 0110011", srl, R, {R(rd) = src1 >> ((uint32_t)src2 & 0x1f);});//逻辑右移
-  INSTPAT("0100000 ????? ????? 101 ????? 0110011", sra, R, {R(rd) = (int32_t)src1 >> ((int32_t)src2 & 0x1f);});//算数右移
+  INSTPAT("0000000 ????? ????? 011 ????? 0110011", sltu, R, { R(rd) = ((uint32_t)src1 < (uint32_t)src2); });
+  INSTPAT("0000000 ????? ????? 100 ????? 0110011", xor, R, { R(rd) = src1 ^ src2; });
+  INSTPAT("0000000 ????? ????? 101 ????? 0110011", srl, R, { R(rd) = src1 >> ((uint32_t)src2 & 0x1f); });         // 逻辑右移
+  INSTPAT("0100000 ????? ????? 101 ????? 0110011", sra, R, { R(rd) = (int32_t)src1 >> ((int32_t)src2 & 0x1f); }); // 算数右移
   INSTPAT("0000000 ????? ????? 110 ????? 0110011", or, R, R(rd) = src1 | src2);
   INSTPAT("0000000 ????? ????? 111 ????? 0110011", and, R, R(rd) = src1 & src2);
 
-  INSTPAT("??????? ????? ????? 000 ????? 0010011", addi,  I, R(rd) = src1 + imm);
-  INSTPAT("??????? ????? ????? 010 ????? 0010011", slti,  I, R(rd) = ((int32_t)src1 < (int32_t)imm));
+  INSTPAT("??????? ????? ????? 000 ????? 0010011", addi, I, R(rd) = src1 + imm);
+  INSTPAT("??????? ????? ????? 010 ????? 0010011", slti, I, R(rd) = ((int32_t)src1 < (int32_t)imm));
   INSTPAT("??????? ????? ????? 011 ????? 0010011", sltiu, I, R(rd) = (uint32_t)src1 < (uint32_t)imm);
-  INSTPAT("??????? ????? ????? 100 ????? 0010011", xori,  I, R(rd) = src1 ^ imm);
-  INSTPAT("??????? ????? ????? 110 ????? 0010011", ori,   I, R(rd) = src1 | imm);
-  INSTPAT("??????? ????? ????? 111 ????? 0010011", andi,  I, R(rd) = src1 & imm);
-  INSTPAT("0000000 ????? ????? 001 ????? 0010011", slli,  I, R(rd) = src1 << (imm & 0x1f));
-  INSTPAT("0000000 ????? ????? 101 ????? 0010011", srli,  I, R(rd) = src1 >> (imm & 0x1f));
-  INSTPAT("0100000 ????? ????? 101 ????? 0010011", srai,  I, R(rd) = (int32_t)src1 >> (imm & 0x1f));
+  INSTPAT("??????? ????? ????? 100 ????? 0010011", xori, I, R(rd) = src1 ^ imm);
+  INSTPAT("??????? ????? ????? 110 ????? 0010011", ori, I, R(rd) = src1 | imm);
+  INSTPAT("??????? ????? ????? 111 ????? 0010011", andi, I, R(rd) = src1 & imm);
+  INSTPAT("0000000 ????? ????? 001 ????? 0010011", slli, I, R(rd) = src1 << (imm & 0x1f));
+  INSTPAT("0000000 ????? ????? 101 ????? 0010011", srli, I, R(rd) = src1 >> (imm & 0x1f));
+  INSTPAT("0100000 ????? ????? 101 ????? 0010011", srai, I, R(rd) = (int32_t)src1 >> (imm & 0x1f));
   // sltiu: 无符号比较立即数
   INSTPAT("??????? ????? ????? 011 ????? 0010011", sltiu, I, R(rd) = src1 < imm);
 
-  INSTPAT("??????? ????? ????? 000 ????? 0000011", lb,  I, R(rd) = SEXT(Mr(src1 + imm, 1), 8));
-  INSTPAT("??????? ????? ????? 001 ????? 0000011", lh,  I, R(rd) = SEXT(Mr(src1 + imm, 2), 16));
-  INSTPAT("????????????  ????? 010 ????? 0000011", lw, I, {R(rd) = Mr((src1 + imm), 4);});
+  INSTPAT("??????? ????? ????? 000 ????? 0000011", lb, I, R(rd) = SEXT(Mr(src1 + imm, 1), 8));
+  INSTPAT("??????? ????? ????? 001 ????? 0000011", lh, I, R(rd) = SEXT(Mr(src1 + imm, 2), 16));
+  INSTPAT("????????????  ????? 010 ????? 0000011", lw, I, { R(rd) = Mr((src1 + imm), 4); });
   INSTPAT("??????? ????? ????? 100 ????? 0000011", lbu, I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 101 ????? 0000011", lhu, I, R(rd) = Mr(src1 + imm, 2));
-  //src1就是R[rs1]
-  INSTPAT("??????? ????? ????? 000 ????? 0100011", sb, S, Mw(src1 + imm, 1, src2));//sw rs2, offset(rs1) [addr] = data, [第一个] = 第三个
-  INSTPAT("??????? ????? ????? 001 ????? 0100011", sh, S, {Mw(src1 + imm,2,src2);});
-  INSTPAT("??????? ????? ????? 010 ????? 0100011", sw, S, {Mw(src1 + imm,4,src2);});
+  // src1就是R[rs1]
+  INSTPAT("??????? ????? ????? 000 ????? 0100011", sb, S, Mw(src1 + imm, 1, src2)); // sw rs2, offset(rs1) [addr] = data, [第一个] = 第三个
+  INSTPAT("??????? ????? ????? 001 ????? 0100011", sh, S, { Mw(src1 + imm, 2, src2); });
+  INSTPAT("??????? ????? ????? 010 ????? 0100011", sw, S, { Mw(src1 + imm, 4, src2); });
 
   INSTPAT("??????? ????? ????? ??? ????? 0010111", auipc, U, R(rd) = s->pc + imm);
-  INSTPAT("????????????????????    ????? 0110111", lui, U, {R(rd) = imm;});
+  INSTPAT("????????????????????    ????? 0110111", lui, U, { R(rd) = imm; });
 
   INSTPAT("????????????  ????? ??? ????? 1101111", jal, J, {
     R(rd) = s->snpc;
     s->dnpc = s->pc + imm;
 #ifdef CONFIG_FUNC_TRACE
-    if(rd == 1)log_ftrace(type_jal, s->pc, s->dnpc);
+    if (rd == 1)
+      log_ftrace(type_jal, s->pc, s->dnpc);
 #endif
   });
   INSTPAT("????????????  ????? 000 ????? 1100111", jarl, I, {
     // 计算跳转目标地址 (src1 是寄存器里的值, imm 是立即数, & ~1 是为了对齐)
     word_t target_ = (src1 + imm) & ~1;
-    
-    R(rd) = s->snpc; 
+
+    R(rd) = s->snpc;
     s->dnpc = target_;
-    
+
 #ifdef CONFIG_FUNC_TRACE
     // 【补全部分】
     // 1. 如果 rd == 1 (ra)，说明把返回地址存进了 ra，这是函数调用 (Call)
-    if (rd == 1) {
-        log_ftrace(type_jalr_call, s->pc, target_);
+    if (rd == 1)
+    {
+      log_ftrace(type_jalr_call, s->pc, target_);
     }
     // 2. 如果 rd == 0 (不保存) 且 rs1 == 1 (源地址来自 ra)，这是函数返回 (Ret)
-    else if (rd == 0 && BITS(s->isa.inst, 19, 15) == 1) {
-        log_ftrace(type_jalr_ret, s->pc, target_);
+    else if (rd == 0 && BITS(s->isa.inst, 19, 15) == 1)
+    {
+      log_ftrace(type_jalr_ret, s->pc, target_);
     }
 #endif
   });
 
-  INSTPAT("??????? ????? ????? 000 ????? 1100011", beq, B, {s->dnpc = src1 == src2 ? s->pc + imm : s-> snpc;});
-  INSTPAT("??????? ????? ????? 001 ????? 1100011", bne, B, {s->dnpc = src1 != src2 ? s->pc + imm : s-> snpc;});
-  INSTPAT("??????? ????? ????? 100 ????? 1100011", blt, B, {s->dnpc = (int32_t)src1 <  (int32_t)src2 ? s->pc + imm : s-> snpc;});
-  INSTPAT("??????? ????? ????? 101 ????? 1100011", bge, B, {s->dnpc = (int32_t)src1 >=  (int32_t)src2 ? s->pc + imm : s-> snpc;});
-  INSTPAT("??????? ????? ????? 110 ????? 1100011", bltu, B, {s->dnpc = (uint32_t)src1 < (uint32_t)src2 ? s->pc + imm : s-> snpc;});
-  INSTPAT("??????? ????? ????? 111 ????? 1100011", bgeu, B, {s->dnpc = (uint32_t)src1 >= (uint32_t)src2 ? s->pc + imm : s-> snpc;});
+  INSTPAT("??????? ????? ????? 000 ????? 1100011", beq, B, { s->dnpc = src1 == src2 ? s->pc + imm : s->snpc; });
+  INSTPAT("??????? ????? ????? 001 ????? 1100011", bne, B, { s->dnpc = src1 != src2 ? s->pc + imm : s->snpc; });
+  INSTPAT("??????? ????? ????? 100 ????? 1100011", blt, B, { s->dnpc = (int32_t)src1 < (int32_t)src2 ? s->pc + imm : s->snpc; });
+  INSTPAT("??????? ????? ????? 101 ????? 1100011", bge, B, { s->dnpc = (int32_t)src1 >= (int32_t)src2 ? s->pc + imm : s->snpc; });
+  INSTPAT("??????? ????? ????? 110 ????? 1100011", bltu, B, { s->dnpc = (uint32_t)src1 < (uint32_t)src2 ? s->pc + imm : s->snpc; });
+  INSTPAT("??????? ????? ????? 111 ????? 1100011", bgeu, B, { s->dnpc = (uint32_t)src1 >= (uint32_t)src2 ? s->pc + imm : s->snpc; });
 
-/* RISCV M EXTENSION        */
-  INSTPAT("0000001 ????? ????? 000 ????? 0110011", mul, R, {R(rd) = src1 * src2;});
-  INSTPAT("0000001 ????? ????? 001 ????? 0110011", mulh, R, R(rd) = ((int64_t)(int32_t)src1*(int64_t)(int32_t)src2) >> 32);
+  /* RISCV M EXTENSION        */
+  INSTPAT("0000001 ????? ????? 000 ????? 0110011", mul, R, { R(rd) = src1 * src2; });
+  INSTPAT("0000001 ????? ????? 001 ????? 0110011", mulh, R, R(rd) = ((int64_t)(int32_t)src1 * (int64_t)(int32_t)src2) >> 32);
   INSTPAT("0000001 ????? ????? 010 ????? 0110011", mulhsu, R, R(rd) = ((int64_t)(int32_t)src1 * (uint64_t)(uint32_t)src2) >> 32;);
-  INSTPAT("0000001 ????? ????? 011 ????? 0110011", mulhu, R, R(rd) = ((uint64_t)(uint32_t)src1 * (uint64_t)(uint32_t)src2) >> 32;);//因为在右移之前， 结果就已经被丢弃了， 所以R(rd) = (src1 * src2) >> 32;不行
+  INSTPAT("0000001 ????? ????? 011 ????? 0110011", mulhu, R, R(rd) = ((uint64_t)(uint32_t)src1 * (uint64_t)(uint32_t)src2) >> 32;); // 因为在右移之前， 结果就已经被丢弃了， 所以R(rd) = (src1 * src2) >> 32;不行
   INSTPAT("0000001 ????? ????? 100 ????? 0110011", div, R, {
     sword_t a = (sword_t)src1;
     sword_t b = (sword_t)src2;
-    if (b == 0) {
-        R(rd) = 0xffffffff; // 除以0，返回 -1
-    } else if (a == 0x80000000 && b == -1) {
-        R(rd) = 0x80000000; // 溢出情况，返回 INT_MIN
-    } else {
-        R(rd) = a / b;      // 正常计算
+    if (b == 0)
+    {
+      R(rd) = 0xffffffff; // 除以0，返回 -1
     }
-});
+    else if (a == 0x80000000 && b == -1)
+    {
+      R(rd) = 0x80000000; // 溢出情况，返回 INT_MIN
+    }
+    else
+    {
+      R(rd) = a / b; // 正常计算
+    }
+  });
   INSTPAT("0000001 ????? ????? 101 ????? 0110011", divu, R, {
-    if (src2 == 0) {
-        R(rd) = 0xffffffff; // 除以0，返回最大值
-    } else {
-        R(rd) = src1 / src2; // 正常计算
+    if (src2 == 0)
+    {
+      R(rd) = 0xffffffff; // 除以0，返回最大值
     }
-});
+    else
+    {
+      R(rd) = src1 / src2; // 正常计算
+    }
+  });
   INSTPAT("0000001 ????? ????? 110 ????? 0110011", rem, R, {
     sword_t rs1 = (sword_t)src1;
     sword_t rs2 = (sword_t)src2;
-    if (rs2 == 0) {
-        R(rd) = rs1;        // 除以0，余数为被除数
-    } else if (rs1 == 0x80000000 && rs2 == -1) {
-        R(rd) = 0;          // 溢出情况，余数为0
-    } else {
-        R(rd) = rs1 % rs2;  // 正常计算
+    if (rs2 == 0)
+    {
+      R(rd) = rs1; // 除以0，余数为被除数
     }
-});
+    else if (rs1 == 0x80000000 && rs2 == -1)
+    {
+      R(rd) = 0; // 溢出情况，余数为0
+    }
+    else
+    {
+      R(rd) = rs1 % rs2; // 正常计算
+    }
+  });
   INSTPAT("0000001 ????? ????? 111 ????? 0110011", remu, R, {
-    if (src2 == 0) {
-        R(rd) = src1;       // 除以0，余数为被除数
-    } else {
-        R(rd) = src1 % src2;// 正常计算
+    if (src2 == 0)
+    {
+      R(rd) = src1; // 除以0，余数为被除数
     }
-});
+    else
+    {
+      R(rd) = src1 % src2; // 正常计算
+    }
+  });
   INSTPAT("??????? ????? ????? 001 ????? 1110011", csrrw, I, {
-    R(rd) = cpu.csr[csr];
+    word_t old = cpu.csr[csr];
     cpu.csr[csr] = src1;
-});// rs1写入csr, csr原先的值写入rd
-//CSR的地址是不经过符号拓展的
-
-
+    R(rd) = old;
+  }); // rs1写入csr, csr原先的值写入rd
+      // CSR的地址是不经过符号拓展的
 
   INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak, N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ???????", inv, N, INV(s->pc));
-  
+
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
@@ -309,59 +324,69 @@ int isa_exec_once(Decode *s)
   return decode_exec(s);
 }
 
-
 #ifdef CONFIG_FUNC_TRACE
-extern FILE* func_log_fp;
+extern FILE *func_log_fp;
 
-static const char* get_func_name(uint32_t addr) {
-  if (funcinfo == NULL || global_func_count == 0) {
-        return "???";
-    }
-    for (int i = 0; i < global_func_count; i++) {
-        uint32_t start = (uint32_t)(uintptr_t)funcinfo[i].addr_begin;
-        uint32_t end   = (uint32_t)(uintptr_t)funcinfo[i].addr_end;
-        
-        if (addr >= start && addr < end) {
-            return funcinfo[i].func_name;
-        }
-    }
+static const char *get_func_name(uint32_t addr)
+{
+  if (funcinfo == NULL || global_func_count == 0)
+  {
     return "???";
+  }
+  for (int i = 0; i < global_func_count; i++)
+  {
+    uint32_t start = (uint32_t)(uintptr_t)funcinfo[i].addr_begin;
+    uint32_t end = (uint32_t)(uintptr_t)funcinfo[i].addr_end;
+
+    if (addr >= start && addr < end)
+    {
+      return funcinfo[i].func_name;
+    }
+  }
+  return "???";
 }
 
-void log_ftrace(int type, uint32_t pc, uint32_t target){
-  if (func_log_fp == NULL) {
-        return;
-    }
+void log_ftrace(int type, uint32_t pc, uint32_t target)
+{
+  if (func_log_fp == NULL)
+  {
+    return;
+  }
   static int call_depth = 0;
-  if (type == type_jal || type == type_jalr_call) {
-        // Call: 查目标地址的函数名
-        const char* target_name = get_func_name(target);
-        
-        fprintf(func_log_fp, "0x%08x:", pc);
-        // 打印缩进和引导线
-        for (int i = 0; i < call_depth; i++) {
-            fprintf(func_log_fp, "  | ");
-        }
-        fprintf(func_log_fp, "call [%s@0x%08x]\n", target_name, target);
-        
-        call_depth++; // 进入下一层
-    } 
-  else if (type == type_jalr_ret) {
-        // Ret: 查当前 PC 地址所在的函数名，代表从哪个函数返回
-        const char* pc_name = get_func_name(pc);
-        
-        if (call_depth > 0) {
-            call_depth--; // 退出当前层
-        }
-        
-        fprintf(func_log_fp, "0x%08x:", pc);
-        // 打印缩进和引导线
-        for (int i = 0; i < call_depth; i++) {
-            fprintf(func_log_fp, "  | ");
-        }
-        fprintf(func_log_fp, "ret  [%s]\n", pc_name);
+  if (type == type_jal || type == type_jalr_call)
+  {
+    // Call: 查目标地址的函数名
+    const char *target_name = get_func_name(target);
+
+    fprintf(func_log_fp, "0x%08x:", pc);
+    // 打印缩进和引导线
+    for (int i = 0; i < call_depth; i++)
+    {
+      fprintf(func_log_fp, "  | ");
     }
-    
-    fflush(func_log_fp);
+    fprintf(func_log_fp, "call [%s@0x%08x]\n", target_name, target);
+
+    call_depth++; // 进入下一层
+  }
+  else if (type == type_jalr_ret)
+  {
+    // Ret: 查当前 PC 地址所在的函数名，代表从哪个函数返回
+    const char *pc_name = get_func_name(pc);
+
+    if (call_depth > 0)
+    {
+      call_depth--; // 退出当前层
+    }
+
+    fprintf(func_log_fp, "0x%08x:", pc);
+    // 打印缩进和引导线
+    for (int i = 0; i < call_depth; i++)
+    {
+      fprintf(func_log_fp, "  | ");
+    }
+    fprintf(func_log_fp, "ret  [%s]\n", pc_name);
+  }
+
+  fflush(func_log_fp);
 }
 #endif
