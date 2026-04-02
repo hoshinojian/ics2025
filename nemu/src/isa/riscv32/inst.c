74ab109d4bc45bc642b41f9c730c81ea5aaa464e
tracer-ics2025 <tracer@njuics.org>
2026-04-02 12:13:44 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:13:44 up 15:58,  1 user,  load average: 0.13, 0.19, 0.20

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -328,9 +328,9 @@ static int decode_exec(Decode *s)
   });
 
   //mret的指责是跳到mepc
-  INSTPAT("0000000 00302 00000 000 00000 1110011", mret, N, {
-    s->dnpc = mepc;
-  });
+  // INSTPAT("0000000 00302 00000 000 00000 1110011", mret, N, {
+  //   s->dnpc = mepc;
+  // });
 
   INSTPAT("0000000 00000 00000 000 00000 1110011", ecall, N, {
     //保存pc
