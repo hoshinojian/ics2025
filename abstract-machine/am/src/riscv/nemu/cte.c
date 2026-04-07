ccdb6169a89cee3ab5d50e6639bda8aa9b17d94a
tracer-ics2025 <tracer@njuics.org>
2026-04-07 17:03:37 +0800

    >  compile NEMU 20010916 xingjian Linux XingjianGaming 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:03:37 up  5:42,  1 user,  load average: 0.18, 0.10, 0.02

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -54,7 +54,7 @@ bool cte_init(Context *(*handler)(Event, Context *))
   // 把asm_trap写到mtvec寄存器里面
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
   // asm volatile("csrw mstatus. %0" : : "r"0xa00001800);
-  asm volatile("csrw mstatus, %0" : : "r" ((uint32_t)0xA00001800));//这是我写的吗。。。。
+  // asm volatile("csrw mstatus, %0" : : "r" ((uint32_t)0xA00001800));//
 
   // mtvec:__am_asm_trap: 完成软件的所有工作, 保存上下文, 调用handler, 切换上下文
 
